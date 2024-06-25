6aee3227ab05338fd9f9e295c491321cb474dc54
Anatoly <kapibarin@DESKTOP-SOKPR1U>
2024-06-25 23:21:21 +0300

    fix: codeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,10 +1,6 @@
 return {
   "Exafunction/codeium.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    require("codeium").setup({})
-  end
+  cmd = "Codeium",
+  build = ":Codeium Auth",
+  opts = {}
 }
